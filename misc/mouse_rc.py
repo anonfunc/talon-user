34948b9859a4b966cc2d5c7600a9a451363ff3bd
Dwight Holman <dwight.holman@workday.com>
2019-01-28 16:25:16 -0800

    Can no longer drive off screen, small acceleration added.

--- misc/mouse_rc.py
+++ misc/mouse_rc.py
@@ -1,13 +1,15 @@
-from talon import canvas, ctrl, ui
+from talon import canvas, ctrl, ui, tap
 from talon.audio import noise
 from talon.voice import Context, ContextGroup, Key
 from talon_plugins import speech
+from talon.track.geom import Point2d
+from talon.track.filter import Acceleration
 
 import time
 from math import radians, pi, sin, cos
 
 SIZE = 10
-SPEED = 300
+SPEED = .5
 
 
 class MouseRcCar:
@@ -16,11 +18,27 @@ class MouseRcCar:
         self.offset_x = self.main_screen.width // 2
         self.offset_y = self.main_screen.height // 2
         self.angle = 0
-        self.speed = 0
+        self.speed = 0.0
         self.mcanvas = canvas.Canvas.from_screen(self.main_screen)
         self.active = False
         self.last_draw = time.time()
+        self.accel = Acceleration(
+            cd=(.001, 100.0),
+            v=(0.0004, 0.0025),
+            lmb=1000.0,
+            ratio=0.3
+        )
+        self.hiss_start = time.time()
         noise.register("noise", self.on_noise)
+        # tap.register(tap.MMOVE, self.on_move)
+
+    # def on_move(self, typ, e):
+    #     if typ != tap.MMOVE or not self.active:
+    #         return
+    #
+    #     # print("moved ", e, self.offset_x, self.offset_y)
+    #     if (e.x, e.y) != (self.offset_x, self.offset_y):
+    #         self.stop(None)
 
     def start(self, *_):
         if self.active:
@@ -39,13 +57,25 @@ class MouseRcCar:
         paint.color = "ff00ff"
         paint.stroke_width = 5
         # print(paint.__dir__())
-        elapsed = time.time() - self.last_draw
+        now = time.time()
+        elapsed = now - self.last_draw
+        hiss_dt = now - self.hiss_start
         c = cos(self.angle)
         s = sin(self.angle)
-        dx = elapsed*self.speed * c
-        dy = elapsed*self.speed * s
-        self.offset_x += dx
-        self.offset_y += dy
+        delta = Point2d(self.speed * c, self.speed * s)
+        # print(delta)
+        delta = delta.apply(self.accel, hiss_dt/100)
+        # print(delta)
+        self.offset_x += delta.x
+        if self.offset_x < 0:
+            self.offset_x = 0
+        elif self.offset_x > self.main_screen.width:
+            self.offset_x = self.main_screen.width
+        self.offset_y += delta.y
+        if self.offset_y < 0:
+            self.offset_y = 0
+        elif self.offset_y > self.main_screen.height:
+            self.offset_y = self.main_screen.height
         ctrl.mouse_move(self.offset_x, self.offset_y)
         line1 = self.rotate(c, s, 0, 0, -2*SIZE, SIZE)
         line2 = self.rotate(c, s, 0, 0, -2*SIZE, -SIZE)
@@ -54,7 +84,7 @@ class MouseRcCar:
         canvas.draw_line(self.offset_x + line1[0], self.offset_y+line1[1], self.offset_x+line1[2],self.offset_y+line1[3])
         canvas.draw_line(self.offset_x + line2[0], self.offset_y+line2[1], self.offset_x+line2[2],self.offset_y+line2[3])
         canvas.draw_line(self.offset_x + line3[0], self.offset_y+line3[1], self.offset_x+line3[2],self.offset_y+line3[3])
-        self.last_draw = time.time()
+        self.last_draw = now
 
     def rotate(self, c, s, x1, y1, x2, y2):
         return x1*c+y1*s, x1*s-y1*c, x2*c+y2*s, x2*s-y2*c
@@ -69,27 +99,25 @@ class MouseRcCar:
                 self.angle += 2*pi
         elif noise == "hiss_start":
             self.speed = SPEED
+            self.hiss_start = time.time()
         elif noise == "hiss_end":
-            self.speed = 0
-
-    def pos(self):
-        return self.offset_x + self.width/2, self.offset_y + self.height/2
+            self.speed = 0.0
 
     def reset(self, _):
         self.offset_x = self.main_screen.width // 2
         self.offset_y = self.main_screen.height // 2
         self.angle = 0
-        self.speed = 0
+        self.speed = 0.0
         self.main_screen = ui.main_screen()
         ctrl.cursor_visible(True)
 
 
-# mg = MouseRcCar()
-# ctx = Context("MouseRcCarStarter")
-# ctx.keymap({
-#     "start driving": [mg.reset, mg.start],
-#     "(done | stop) driving": mg.stop,
-#     # "snap done": [mg.stop, lambda _: ctx.unload()],
-# })
+mg = MouseRcCar()
+ctx = Context("MouseRcCarStarter")
+ctx.keymap({
+    "start driving": [mg.reset, mg.start],
+    "(done | stop) driving": mg.stop,
+    # "snap done": [mg.stop, lambda _: ctx.unload()],
+})
 
 #mg.start(None)
